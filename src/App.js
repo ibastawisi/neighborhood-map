c7c0de7fe560cde789f335663b698bb8aa2afaac
ibrahim-1996 <ibrahim.1996@outlook.com>
2018-06-01 17:18:57 +0200

    Add markers, filter locations

--- src/App.js
+++ src/App.js
@@ -2,20 +2,38 @@ import React, { Component } from 'react';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.min.js';
+import escapeRegExp from 'escape-string-regexp'
 import MapContainer from './MapContainer'
-import Locations from './Locations';
+import Nav from './Nav';
 
 class App extends Component {
+  state = {
+    locations: [],
+    query: '',
+    filteredlocs: []
+  }
+  componentDidMount() {
+    fetch('loc.json')
+      .then(response => response.json()).then(locations => this.setState({
+        locations: [...locations],
+        filteredlocs: [...locations]
+      }))
+  }
 
+  filter = (query) => {
+    this.setState({ query: query.trim() })
+    const match = new RegExp(escapeRegExp(query), 'i')
+    this.setState({ filteredlocs: this.state.locations.filter((location) => match.test(location.name)) || this.state.locations })
+
+  }
   render() {
     return (
       <div className="container-fluid">
         <div className="row flex-column flex-lg-row">
-          <Locations />
-          <MapContainer google={this.props.google} />
+          <Nav locations={this.state.filteredlocs} query={this.state.query} filter={this.filter} />
+          <MapContainer google={this.props.google} locations={this.state.filteredlocs || this.state.locations} />
         </div>
       </div>
-
     )
   }
 }
--- src/MapContainer.js
+++ src/MapContainer.js
@@ -2,12 +2,51 @@ import React, { Component } from 'react';
 import { Map, InfoWindow, Marker, GoogleApiWrapper } from 'google-maps-react';
 
 export class MapContainer extends Component {
-      render() {
+    state = {
+        showingInfoWindow: false,
+        activeMarker: {},
+        selectedPlace: {},
+    };
+
+    onMarkerClick = (props, marker, e) =>
+        this.setState({
+            selectedPlace: props,
+            activeMarker: marker,
+            showingInfoWindow: true
+        });
+
+    onMapClicked = (props) => {
+        if (this.state.showingInfoWindow) {
+            this.setState({
+                showingInfoWindow: false,
+                activeMarker: null
+            })
+        }
+    };
+
+    render() {
+        const locations = this.props.locations
         return (
-            <Map className="col-lg-9 map-container" google={this.props.google}initialCenter={{
+            <Map className="col-lg-9 map-container" google={this.props.google} initialCenter={{
                 lat: 27.5,
                 lng: 31.5
-              }} zoom={6.3}></Map>
+            }} zoom={6.3} onClick={this.onMapClicked}>
+                {locations.map(location =>
+                    <Marker
+                        key={location.name}
+                        onClick={this.onMarkerClick}
+                        name={location.name}
+                        title={location.name}
+                        position={{ lat: location.pos[0], lng: location.pos[1] }} />
+                )}
+                <InfoWindow
+                    marker={this.state.activeMarker}
+                    visible={this.state.showingInfoWindow}>
+                    <div>
+                        <h5>{this.state.selectedPlace.name}</h5>
+                    </div>
+                </InfoWindow>
+            </Map>
         )
     }
 }
